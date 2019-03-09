89736222acde52d2383919781231f00c43a21951
lizqwerscott <m13994059075@163.com>
2019-03-09 17:25:06 +0800

    Fix load the table.

--- file-manager/file-manager.lisp
+++ file-manager/file-manager.lisp
@@ -30,7 +30,7 @@
      :initarg :attributes
      :reader table-attributes)
    (date
-     ;:initarg :date
+     :initarg :date
      :accessor table-date)
    (come-from
      :initarg :come-from
@@ -66,41 +66,46 @@
       (with-standard-io-syntax 
         (print plist out))) plist))
 
-;;;todo load-table
-(defmethod load-table ((table-one table)))
 
-(add-table "hello" "http://baidu.com" "Video" "LingMengYuShuo" "The test" "~/Documents/test-web/Downloads/test" "~/Documents/test-web/Downloads/test.rar")
+;(add-table "hello" "http://baidu.com" "Video" "LingMengYuShuo" "The test" "~/Documents/test-web/Downloads/test" "~/Documents/test-web/Downloads/test.rar")
 
 ;(vector-push-extend 'a (gethash "Video" *table-manager-hash*))
-(setf (gethash "Video" *table-manager-hash*) (remove 'a (gethash "Video" *table-manager-hash*)))
+;(setf (gethash "Video" *table-manager-hash*) (remove 'a (gethash "Video" *table-manager-hash*)))
 ;(setf (gethash "Video" *table-manager-hash*) (make-array 10 :fill-pointer 0 :adjustable t))
 
-(remove-table "hello" "Video")
-(save-table (find "hello" (gethash "Video" *table-manager-hash*) :test #'string= :key #'(lambda (table-one) (table-name table-one))))
+;(table-path (elt (gethash "Video" *table-manager-hash*) 0)) 
+;(remove-table "hello" "Video")
+;(save-table (find "hello" (gethash "Video" *table-manager-hash*) :test #'string= :key #'(lambda (table-one) (table-name table-one))))
 
-(defmethod initialize-instance :after ((table-one table) &key b-path y-path)
-  (make-table-dir table-one)
-  (move-table table-one b-path y-path)
+(defmethod initialize-instance :after ((table-one table) &key loadp b-path y-path)
+  (when loadp 
+    (make-table-dir table-one)
+    (move-table table-one b-path y-path)) 
   (setf (table-date table-one) "2019.2.17.22:10"))
 
-(defun load-table-group (key)
-  (setf (gethash key *table-manager-hash*) (make-array 10 :fill-pointer 0 :adjustable t))
-  (let ((path (merge-pathnames (pathname (format nil "~a/" key)) *drive-path*))
+(defun load-table (path)
+  (let* ((plist (with-open-file (in (merge-pathnames (make-pathname :name "info" :type "txt") path)) (with-standard-io-syntax (read in))))
+         (table-one (make-instance 'table :name (getf plist :name) :url (getf plist :url) :attributes (getf plist :attributes) :come-from (getf plist :come-from) :description (getf plist :description) :b-path (getf plist :b-path) :y-path (getf plist :y-path) :date (getf plist :date) :loadp nil)))
+    (setf (table-path table-one) (getf plist :path)) table-one))
+
+(defun load-table-group (path)
+  (let* ((key (car (last (pathname-directory path))))
         (table-group (gethash key *table-manager-hash*))) 
+    (setf (gethash key *table-manager-hash*) (make-array 10 :fill-pointer 0 :adjustable t))
     (if (not (probe-file path))
       (ensure-directories-exist path)
-      (dolist (table-one-path (directory (merge-pathnames (make-pathname :name :wild :type :wild) path)))
-        ()
-        ))))
+      (dolist (table-come-path (directory (merge-pathnames (make-pathname :name :wild :type :wild) path)))
+        (dolist (table-one-path (directory (merge-pathnames (make-pathname :name :wild :type :wild) table-come-path))) 
+          (vector-push (load-table table-one-path) (gethash key *table-manager-hash*)))))))
 
 (defun load-table-manager ()
-  ()
-  )
+  (dolist (table-one-group (directory (merge-pathnames (make-pathname :name :wild :type :wild) *drive-path*)))
+    (when (not (string= (car (last (pathname-directory table-one-group))) "Downloads")) (load-table-group table-one-group))))
 
-(defun add-table (name url attributes come-from description b-path y-path)
-  (vector-push 
-    (make-instance 'table :name name :url url :attributes attributes :come-from come-from :description description :b-path b-path :y-path y-path)
-   (gethash attributes *table-manager-hash*)))
+(defun add-table (name url attributes come-from description b-path y-path &optional (date nil))
+  (let (table-one (make-instance 'table :name name :url url :attributes attributes :come-from come-from :description description :b-path b-path :y-path y-path :date date :loadp t))
+    (vector-push table-one (gethash attributes *table-manager-hash*))
+    (save-table table-one) table-one))
 
 (defun remove-table (name attributes)
   (run-shell (format nil "rm -rf ~a" (namestring (table-path (find name (gethash attributes *table-manager-hash*) :key #'(lambda (table-one) (table-name table-one)) :test #'string=)))) t)
@@ -111,10 +116,11 @@
                            (table-name table-one)) 
           :test #'string=)))
 
-(load-table-group "Video")
-(load-table-group "Music")
-(load-table-group "Game")
-(load-table-group "MMD")
+(load-table-manager)
+;(load-table-group "Video")
+;(load-table-group "Music")
+;(load-table-group "Game")
+;(load-table-group "MMD")
 
 (in-package :cl-user)
 
