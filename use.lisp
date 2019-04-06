34a243080002dbb45903c00343acb89c58b0d5e6
lizqwerscott <m13994059075@163.com>
2019-04-06 12:33:50 +0800

    A fix

--- use.lisp
+++ use.lisp
@@ -9,7 +9,7 @@
 (load "~/Documents/Learn/Lisp/TheWebApp/file-manager/file-manager.lisp")
 (load "~/Documents/Learn/Lisp/TheWebApp/head.lisp")
 
-(load "./web-manager.asd")
+;(load "./web-manager.asd")
 (asdf:operate 'asdf:load-op 'web-manager)
 
 (web-manager:add-task "baidu" "http://baidu.com")
