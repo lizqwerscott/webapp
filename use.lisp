8e569be641c8c6ae89e95ec18939a47778ae3b24
lizqwerscott <m13994059075@163.com>
2019-04-06 13:08:47 +0800

    fix

--- use.lisp
+++ use.lisp
@@ -6,10 +6,10 @@
 (ql:quickload "cl-json") ;load json
 (ql:quickload "websocket-driver") ;load websocket
 
-(load "~/Documents/Learn/Lisp/TheWebApp/file-manager/file-manager.lisp")
-(load "~/Documents/Learn/Lisp/TheWebApp/head.lisp")
+;(load "~/Documents/Learn/Lisp/TheWebApp/file-manager/file-manager.lisp")
+;(load "~/Documents/Learn/Lisp/TheWebApp/head.lisp")
 
-;(load "./web-manager.asd")
+(load "./web-manager.asd")
 (asdf:operate 'asdf:load-op 'web-manager)
 
 (web-manager:add-task "baidu" "http://baidu.com")
