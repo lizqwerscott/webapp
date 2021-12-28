cdac5749b53db2455da39565907efb205dcb78c3
lizqwerscott <lizqwerscott@163.com>
2021-12-28 19:55:34 +0800

    add test

--- use.lisp
+++ use.lisp
@@ -1,22 +1,4 @@
-(in-package :cl-user)
-#+clozure 
-(load "/home/pi/quicklisp/setup.lisp")
-#+sbcl
-(ql:quickload "asdf")
-
-(ql:quickload "bordeaux-threads")
-(ql:quickload "cl-strings")
-;(ql:quickload "cl-events") ;load event
-;(ql:quickload "lparallel") ;load thread pool manager
-;(ql:quickload "cl-json") ;load json
-;(ql:quickload "websocket-driver") ;load websocket
-(asdf:load-system :uiop)
-
-;(load "~/Documents/Learn/Lisp/TheWebApp/file-manager/file-manager.lisp")
-;(load "~/Documents/Learn/Lisp/TheWebApp/head.lisp")
-
-(load "./web-manager.asd")
-(asdf:operate 'asdf:load-op 'web-manager)
+(ql:quickload :web-manager)
 
 (defun test-baidu ()
   (web-manager:add-task (list :id "baidu" :url "https://baidu.com" :attributes "Video" :come-from "test" :description "baidu" :download-type "common" :extractp :zipp t :password " ")))
@@ -26,6 +8,17 @@
 
 (defun test-mo () 
   (web-manager:add-task (list :id "mo" :url "file://~/tets-web/Downlaod/" :attributes "Game" :come-from "mcbbs" :description "mcsever" :download-type "local" :extractp :zipp t :password "nil")))
+
+(defun test-bt ()
+  (web-manager:add-task (list :id "archlinux"
+                              :url "magnet:?xt=urn:btih:287ce5be250c6613ab6021c4a483cbfc672683f8&dn=archlinux-2021.12.01-x86_64.iso"
+                              :tag "anime"
+                              :description "archlinux"
+                              :attributes "video"
+                              :download-type "bt"
+                              :r18p nil
+                              :removep t
+                              :password "nil")))
 ;(web-manager:add-task "baidu" "http://baidu.com")
 ;(web-manager:get-task-download-info (find-task "baidu") :status)
 ;(web-manager:add-task (list :id "S1534" :url "local" :attributes "Video" :come-from "LingMeiYushuo" :description "LingMeng2" :download-type "local" :zipp t :password "nil"))
